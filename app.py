5eaa47faa446cb015958d5653b2ddfd43801c922
Trish Ladd <4132874+TrishZwei@users.noreply.github.com>
2020-12-16 07:14:41 -0800

    Made suggested changes
    tried suggested code, mades some additional changes.Still not working.
    
    Co-Authored-By: jm1021 <54686997+jm1021@users.noreply.github.com>

--- app.py
+++ app.py
@@ -27,29 +27,41 @@ def article(id):
 
 @app.route('/joke', methods=['GET', 'POST'])
 def joke():
-	#variables created as empty strings as we'll be getting back some string data from the url.
-	setup = 'this is the default setup'
-	punchline = 'this is the default punchline'
-	#values should be overwritten below:
-	if request.method == 'POST':
-		url = 'https://official-joke-api.appspot.com/jokes/programming/random'
-		resp = requests.get(url)
-		''' 
-		#this is not working either
-		j_string = resp.json()
-		setup = j_string[0].setup
-		punchline = j_string[0].punchline
-		'''
+	url = 'https://official-joke-api.appspot.com/jokes/programming/random'
+	resp = requests.get(url)
+	'''
+	tried resp=requests.get(url) got following error:
+	NameError: name 'requests' is not defined (line 31 )
 
-		'''		
-		#is there more than one item? No... then why the loop?
-		for item in resp.json():
-			setup = item['setup']
-			punchline = item['punchline'] 
-		'''
+	tried resp=request.get(url) got following error:
+	AttributeError: 'Request' object has no attribute 'get'
+	this looks like a better error to me because it is at least returning an object.
+
+	tried resp = request.json.get(url)
+	AttributeError: 'NoneType' object has no attribute 'get'
+
+	tried resp = request.json().get(url)
+	TypeError: 'NoneType' object is not callable
+
+	'''
+	setup = resp.json()[0]['setup']
+	punchline = resp.json()[0]['punchline']
 
 	return render_template('joke.html', setup = setup, punchline = punchline)
 
+	''' 
+	#this is not working either
+	j_string = resp.json()
+	setup = j_string[0].setup
+	punchline = j_string[0].punchline
+	'''
+
+	'''		
+	#is there more than one item? No... then why the loop?
+	for item in resp.json():
+		setup = item['setup']
+		punchline = item['punchline'] 
+	'''
 
 if __name__ == '__main__':
 	 app.run(); #host and port can be added into parameters
